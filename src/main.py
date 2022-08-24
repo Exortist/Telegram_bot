1bfaebb96d8a44eb5f680161a14853c39b43c852
Avtukhov Dmitry <avtukhov.d@web-canape.ru>
2022-08-24 11:52:46 +0300

    init commit

---
+++ src/main.py
@@ -0,0 +1,360 @@
+import requests
+import logging
+import os
+import io
+from urllib import request
+from bs4 import BeautifulSoup
+import telebot
+from time import sleep
+from loguru import logger
+import json
+from utils import (
+    is_admin,
+    get_id,
+    get_hosts,
+    get_templates,
+    run_template_for_host,
+    callback_data_dumps,
+    confirm_action,
+    get_stdout,
+)
+from telebot import types
+
+
+class Main:
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    awxReadToken = os.getenv("AWX_READ_TOKEN")
+    awxWriteToken = os.getenv("AWX_WRITE_TOKEN", default="")
+    awxUrl = os.getenv("AWX_URL")
+    headers = {
+        "Authorization": "Bearer " + awxWriteToken,
+        "content-type": "application/json",
+    }
+    users = os.getenv("USERS").split(';')
+    templates = os.getenv("TEMPLATES").split(';')
+    
+    page = 1
+    
+    def __init__(self):
+        
+        self.bot = telebot.TeleBot(self.token)
+        
+        logger.debug("Init")
+        print(type(self.users))
+        
+
+    def startBot(self):
+
+        logger.debug("__main__")
+
+        @self.bot.message_handler(commands=["start"])
+        def _proccess_command_start(msg):
+            
+            self.start(msg)
+
+        @self.bot.message_handler(commands=["menu"])
+        def _proccess_command_menu(msg):
+            self.menu(msg)
+
+        @self.bot.message_handler(commands=["getID"])
+        def _proccess_command_printid(msg):
+            self.print_id(msg)
+
+        @self.bot.callback_query_handler(func=lambda call: True)
+        def _proccess_callback_query(call):
+            data = json.loads(call.data)
+            queryHeader = data[0]
+
+            match queryHeader:
+                case "unsee":
+                    logger.debug("unsee")
+                    self.bot.delete_message(
+                        call.message.chat.id, call.message.message_id
+                    )
+                case "next":
+                    try:
+                        self.page += 1
+                        list_hosts = self.list_hosts(call, self.page)
+                        self.bot.edit_message_text(
+                            text=list_hosts.get("text"),
+                            reply_markup=list_hosts.get("markup"),
+                            chat_id=call.message.chat.id,
+                            message_id=call.message.message_id,
+                        )
+                    except Exception as e:
+                        logger.error(e)
+                        self.bot.send_message(
+                            call.message.chat.id, "Не удаётся получить список хостов"
+                        )
+                        return
+                case "previous":
+                    try:
+                        self.page -= 1
+                        list_hosts = self.list_hosts(call, self.page)
+                        self.bot.edit_message_text(
+                            text=list_hosts.get("text"),
+                            reply_markup=list_hosts.get("markup"),
+                            chat_id=call.message.chat.id,
+                            message_id=call.message.message_id,
+                        )
+                    except Exception as e:
+                        logger.error(e)
+                        self.bot.send_message(
+                            call.message.chat.id, "Не удаётся получить список хостов"
+                        )
+                        return
+                case "hostTemplates":
+                    try:
+                        
+                        templates = self.select_template(call)
+                        self.bot.edit_message_text(
+                            text=templates.get("text"),
+                            reply_markup=templates.get("markup"),
+                            parse_mode="html",
+                            chat_id=call.message.chat.id,
+                            message_id=call.message.message_id,
+                        )
+                    except Exception as e:
+                        logger.error(e)
+                        self.bot.send_message(
+                            call.message.chat.id, "Не удаётся получить список хостов"
+                        )
+                        return
+
+                case "backToServerList":
+                    try:
+                        list_hosts = self.list_hosts(call, self.page)
+                        self.bot.edit_message_text(
+                            text=list_hosts.get("text"),
+                            reply_markup=list_hosts.get("markup"),
+                            chat_id=call.message.chat.id,
+                            message_id=call.message.message_id,
+                        )
+                    except Exception as e:
+                        logger.error(e)
+                        self.bot.send_message(
+                            call.message.chat.id, "Не удаётся получить список хостов"
+                        )
+                        return
+                case "runTemplateForHost":
+                    self.run_template_for_host(call)
+                case "runTemplate":
+                    # runTemplate()
+                    pass
+                case "selectPlaybookForHost":
+                    self.hosts(call.message)
+
+        self.bot.polling(none_stop=True)
+
+    def start(self, msg):
+        if is_admin(msg.from_user.id, self.users):
+            self.bot.send_message(msg.chat.id, "Вы <b>АДМИНИСТРАТОР</b>", "html")
+            self.menu(msg)
+
+        else:
+            self.bot.send_message(msg.chat.id, "Вы не <b>АДМИНИСТРАТОР</b>", "html")
+            self.bot.send_message(msg.chat.id, "/getID -  Получить ID")
+
+    def menu(self, msg):
+        if is_admin(not msg.from_user.id, self.users):
+            self.bot.send_message(msg.chat.id, "Вы не администратор")
+            self.print_id(msg)
+            return
+        menu_text = "Выберите действие"
+
+        host_button = types.InlineKeyboardButton(
+            text="Запустить Playbook для конкретного Хоста",
+            callback_data=callback_data_dumps(["selectPlaybookForHost"]),
+        )
+        markup = types.InlineKeyboardMarkup()
+        markup.add(host_button)
+        self.bot.send_message(
+            msg.chat.id, menu_text, reply_markup=markup, parse_mode="html"
+        )
+
+    def hosts(self, msg):
+        if is_admin(not msg.from_user.id, self.users):
+            return
+
+        try:
+            list_hosts = self.list_hosts(msg, self.page)
+            self.bot.send_message(
+                msg.chat.id,
+                list_hosts.get("text"),
+                reply_markup=list_hosts.get("markup"),
+                parse_mode="html",
+            )
+        except Exception as e:
+            logger.error(e)
+            self.bot.send_message(msg.chat.id, "Не удаётся получить список хостов")
+            return
+
+    def list_hosts(self, msg, pg):
+        buttons = []
+        navButtons = []
+
+        navButtons.append(
+            types.InlineKeyboardButton(
+                text="Закрыть",
+                row_width=3,
+                callback_data=callback_data_dumps(["unsee"]),
+            )
+        )
+
+        markup = types.InlineKeyboardMarkup()
+        try:
+            results = get_hosts("GET", self.awxUrl, self.headers, page_id=pg)
+        except Exception as e:
+            logger.error(e)
+            self.bot.send_message(msg.chat.id, "Не удаётся получить список хостов")
+            return
+
+        if results["next"] is not None:
+            navButtons.append(
+                types.InlineKeyboardButton(
+                    text="Вперёд", callback_data=callback_data_dumps(["next"])
+                )
+            )
+        if results["previous"] is not None:
+            navButtons.append(
+                types.InlineKeyboardButton(
+                    text="Назад", callback_data=callback_data_dumps(["previous"])
+                )
+            )
+
+        text = f"Всего хостов: <b>{results['count']}</b>\nСписок хостов:\n"
+        for host in results["results"]:
+
+            text += f'{host["id"]}. {host["name"]}\n'
+            callback_dict = ["hostTemplates", {"h_i": host["id"], "h_n": host["name"]}]
+            buttons.append(
+                types.InlineKeyboardButton(
+                    text=f'{host["name"]}', callback_data=str(json.dumps(callback_dict))
+                )
+            )
+
+        markup.add(*navButtons, *buttons)
+        return {"text": text, "markup": markup}
+
+    def select_template(self, call):
+        if is_admin(not call.from_user.id, self.users):
+            self.bot.send_message(call.chat.id, "Вы не администратор")
+            return
+        
+        data = json.loads(call.data)
+        host = data[1]
+        text = f"""Вы выбрали Хост: <b>{host['h_n']}</b>\nСписок шаблонов
+ID  NAME\n"""
+        callback_dict = []
+
+        buttons = []
+        templates = self.templates
+        try:
+            markup = types.InlineKeyboardMarkup()
+            back_button = types.InlineKeyboardButton(
+                text="Вернуться",
+                callback_data=str(json.dumps(["backToServerList"])),
+            )
+           
+            for template in get_templates("GET", self.awxUrl, headers=self.headers)[
+                "results"
+            ]:
+                
+                if str(template["id"]) in templates:
+                    
+                    text += f"{str(template['id'])} {template['name']}\n"
+                    callback_dict = [
+                        "runTemplateForHost",
+                        {"h_n": host["h_n"], "t_i": template["id"]},
+                    ]
+
+                    buttons.append(
+                        types.InlineKeyboardButton(
+                            text=f'Шаблон №{template["id"]}',
+                            callback_data=str(json.dumps(callback_dict)),
+                        )
+                    )
+
+            markup.add(*buttons, back_button)
+
+            return {"text": text, "markup": markup}
+        except Exception as e:
+            logger.error(e)
+            self.bot.send_message(call.message.chat.id, "Не удаётся выбрать шаблон")
+            return
+
+    def run_template_for_host(self, data):
+
+        if is_admin(not data.from_user.id, self.users):
+            self.bot.send_message(data.message.chat.id, "Вы не администратор")
+            return
+
+        template_data = json.loads(data.data)
+        chat_id = data.message.chat.id
+        try:
+            requests.request("GET", self.awxUrl, headers=self.headers)
+        except Exception as e:
+            logging.error(e)
+            self.bot.send_message(chat_id, "Не удаётся подключиться к серверу")
+            return
+
+        obj = confirm_action()
+
+        img = self.bot.send_photo(chat_id, obj.get("capcha_img"))
+
+        self.bot.register_next_step_handler(
+            img, lambda m: self.run(m, data, template_data, obj)
+        )
+
+    def run(self, m, data, template_data, obj):
+
+        chat_id = data.message.chat.id
+        template_id = template_data[1].get("t_i")
+        host_name = template_data[1].get("h_n")
+        host = {"limit": host_name}
+
+        try:
+            if obj.get("capcha_num") == str(m.text):
+                res = run_template_for_host(
+                    "POST",
+                    template_id,
+                    self.awxUrl,
+                    headers=self.headers,
+                    data=host,
+                ).text
+                # os.remove(f"./capcha_{chat_id}.tmp")
+                sleep(10)
+                self.bot.send_message(chat_id, "Шаблон Выполнился Успешно\n")
+
+                html = get_stdout(
+                    self.awxUrl,
+                    headers=self.headers,
+                    page_id=json.loads(res).get("id"),
+                )
+                soup = BeautifulSoup(html, "html.parser")
+
+                stdout = soup.findAll("div", {"class": "response-info"})
+
+                file_obj = io.BytesIO(stdout[0].encode())
+
+                file_obj.name = "stdout.html"
+                self.bot.send_document(chat_id, file_obj)
+
+                logger.debug(
+                    f"Пользователь {data.from_user.username} запустил шаблон {template_id} на хосте {host_name}"
+                )
+
+            else:
+
+                self.bot.send_message(
+                    chat_id, "Капча введена не верно\nШаблон Не выполнился\n"
+                )
+        except Exception as e:
+            print(e)
+            self.bot.send_message(chat_id, "Произошла ошибка")
+
+    def print_id(self, msg):
+        self.bot.send_message(msg.chat.id, "ID Пользователя - " + str(msg.from_user.id))
+
+
+Main().startBot()
---
+++ src/utils.py
@@ -0,0 +1,67 @@
+import json
+import random
+import pyfiglet
+from requests import request
+from PIL import Image, ImageDraw
+
+
+
+
+def is_admin(user_id, users):
+    
+    if str(user_id) in users:
+        return True
+    return False
+
+def get_id(message):
+    return message.from_user.id
+
+
+def get_hosts(method, url, headers, page_id=1, host_id=""):
+    return json.loads(
+        request(method, url + f"/api/v2/hosts/?page={page_id}", headers=headers).text
+    )
+
+
+def get_templates(method, url, headers):
+    return json.loads(
+        request(method, url + "/api/v2/job_templates/", headers=headers).text
+    )
+
+
+def get_stdout(url, headers, page_id):
+    return request("GET", url + f"/api/v2/jobs/{page_id}/stdout/", headers=headers).text
+
+
+def run_template(method, url, headers, inventory_json):
+    return request(
+        method,
+        url + "/api/v2/inventories/2/ad_hoc_commands/",
+        headers=headers,
+        json=inventory_json,
+    ).text
+
+
+def run_template_for_host(method, template_id, url, headers, data):
+    return request(
+        method,
+        url + f"/api/v2/job_templates/{template_id}/launch/",
+        headers=headers,
+        data=json.dumps(data),
+    )
+
+
+def callback_data_dumps(json_content):
+    return json.dumps(json_content)
+
+
+def confirm_action():
+    check_number = random.randint(0, 9999)
+    check_string = f"{check_number:>04}"
+    print(pyfiglet.Figlet().renderText(check_string))
+    image = Image.new("RGB", (150, 100), color=("#fff"))
+    draw_text = ImageDraw.Draw(image)
+    draw_text.text(
+        (10, 10), pyfiglet.Figlet().renderText(check_string), fill=("#1C0606")
+    )
+    return {"capcha_img": image, "capcha_num": str(check_number)}
